927450f4139b29e3e6732f4e4f7ea0b888e107cd
aleprime2007 <aleprime2007@gmail.com>
2023-03-01 16:55:32 -0400

    Alpha 0.1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//-------------------------Variables-------------------------\\
+let slots = ["", "", "", "", "", "", "", "", ""]
+
+turnoCirculos = 1
+turnoEquis = 0
+
+let circulos = [document.getElementById("circulo1"), document.getElementById("circulo2"), document.getElementById("circulo3"), document.getElementById("circulo4"), document.getElementById("circulo5"), document.getElementById("circulo6"), document.getElementById("circulo7"), document.getElementById("circulo8"), document.getElementById("circulo9"), ]
+let equis = [document.getElementById("equis1"), document.getElementById("equis2"), document.getElementById("equis3"), document.getElementById("equis4"), document.getElementById("equis5"), document.getElementById("equis6"), document.getElementById("equis7"), document.getElementById("equis8"), document.getElementById("equis9"), ]
